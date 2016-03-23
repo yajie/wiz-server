0c7e6d4b0d109b420c0dd8706486ea4aba039832
Peter Wu <yajie.wu@hotmail.com>
2016-03-23 21:36:50 +0800

    wiz-server jpg update

--- wizweb/wiz/migrations/0002_auto_20150420_2144.py
+++ wizweb/wiz/migrations/0002_auto_20150420_2144.py
@@ -5,17 +5,29 @@ from django.db import models, migrations
 from wiz.models import News
 
 def init_news(apps, schema_editor):
-    ethonalItem = News()
-    ethonalItem.data_index = 0
-    ethonalItem.year = 2015
-    ethonalItem.message = "Ethanol & Biofuels Asia 2015 will be held on July 8-9,in Manila Philippines."
-    ethonalItem.save()
+    firstItem = News()
+    firstItem.data_index = 0
+    firstItem.year = 2016
+    firstItem.message = "Ethanol & Biofuels Asia 2016 will be held in Singapore during June 29-30."
+    firstItem.save()
 
-    soyaItem = News()
-    soyaItem.data_index = 1
-    soyaItem.year = 2015
-    soyaItem.message = "Soya Asia is coming."
-    soyaItem.save()
+    secondItem = News()
+    secondItem.data_index = 1
+    secondItem.year = 2016
+    secondItem.message = "Ethanol Outlook India 2016 will be held in Mumbai during August 19-20."
+    secondItem.save()
+
+    thirdItem = News()
+    thirdItem.data_index = 2
+    thirdItem.year = 2016
+    thirdItem.message = "Wiz is now one of the representatives of UL Prospector®- the leading Raw Materials Search Engine"
+    thirdItem.save()
+
+    fourthItem = News()
+    fourthItem.data_index = 3
+    fourthItem.year = 2016
+    fourthItem.message = "WoodChem 2016 will be held in Bangkok in October 19-20"
+    fourthItem.save()
 
 class Migration(migrations.Migration):
 
